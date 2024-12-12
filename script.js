a41b610922c0a0506c7fe4b12a1a604f3366965b
Sireesha <krishnasaisireesha@gmail.com>
2024-12-12 15:34:51 +0530

    My To Do List

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const todoList = document.getElementById('todoList');
 
 const addTodo = ()=>{
      const inputText = inputBox.value.trim();
+     //trim= it'll remove only first and last spaces
+     
      if(inputText.length <= 0){
         alert("You must write somethoing in your to do")
      }
