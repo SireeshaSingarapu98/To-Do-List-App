7b5eaca0dd328bf3bc39a26d463876438dd14334
Sireesha <krishnasaisireesha@gmail.com>
2024-12-12 15:24:14 +0530

    To Do List

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputBox = document.getElementById('inputBox');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+  
+
+
+
+const addTodo = ()=>{
+     const inputText = inputBox.value.trim();
+     if(inputText.length <= 0){
+        alert("You must write somethoing in your to do")
+     }
+
+     const li = document.createElement("li");
+     const p = document.createElement("p");
+     p.innerHTML = inputText;
+     li.appendChild(p);
+
+     todoList.appendChild(li);
+     inputBox.value = "";
+}
+
+addBtn.addEventListener('click', addTodo);
+
+inputBox.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {
+        addTodo();
+    }
+});
